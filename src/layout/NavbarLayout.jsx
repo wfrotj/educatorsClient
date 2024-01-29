e01844f3f3855ed27cde1ef1e67d3bf3263fdea4
wfrotj <trebrodrigo@gmail.com>
2024-01-30 00:51:49 +0800

    layout

--- src/layout/NavbarLayout.jsx
+++ src/layout/NavbarLayout.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import teacherService from "../services/teacherService";
 
 function NavbarLayout({ user }) {
   return (
--- src/layout/ProfileLayout.jsx
+++ src/layout/ProfileLayout.jsx
@@ -25,10 +25,7 @@ function ProfileLayout({ user, setUser }) {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
               magni facere voluptates tempore quae ea sint magnam minus quasi
               repellat. Aspernatur doloribus mollitia error! Iste dolore
-              explicabo a velit harum! Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Commodi ipsum illo unde ipsa nostrum aspernatur
-              atque voluptatibus libero delectus ex. Vel nihil provident nobis
-              corporis, earum cum quod quisquam delectus!
+              explicabo a velit harum!
             </p>
           </div>
           <div className="profile-buttons">
