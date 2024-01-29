c725184366c110475ab7ab6081911c50ed70e5ad
wfrotj <trebrodrigo@gmail.com>
2024-01-30 00:11:22 +0800

    edit config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  /*   server: {
+    proxy: "http://localhost:3005",
+  }, */
+});
