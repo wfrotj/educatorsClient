230d90b42d46f4e2004b0cd6532e64342b028316
wfrotj <trebrodrigo@gmail.com>
2024-01-30 00:32:31 +0800

    edit layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home({ user, setUser }) {
   return (
     <div>
       <h1>This is the homepage</h1>
+      <p>Welcome {user}</p>
       <section>
         <iframe
           className="iframe-school"
