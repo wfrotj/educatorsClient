def0dfb1defc61d73654029458c558fea8527122
wfrotj <trebrodrigo@gmail.com>
2024-01-30 00:36:02 +0800

    editpages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ function Home({ user, setUser }) {
   return (
     <div>
       <h1>This is the homepage</h1>
-      <p>Welcome {user}</p>
+      <p>Welcome back {user}</p>
+
       <section>
         <iframe
           className="iframe-school"
